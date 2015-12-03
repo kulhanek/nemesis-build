4261595bfe5a6426b0bbaf929bea61ce22183893
Petr Kulhanek <kulhanek@chemi.muni.cz>
2015-12-03 16:51:01 +0100

    win/unx compilation

--- 02.push-code.sh
+++ 02.push-code.sh
@@ -2,21 +2,21 @@
 
 # ------------------------------------------------------------------------------
 function push_code() {
-echo ""
-echo "# $2 -> $1"
-echo "# -------------------------------------------" 
-OLDPWD=$PWD
-cd $1 || exit 1
-git push github master
-cd $OLDPWD
+    echo ""
+    echo "# $2 -> $1"
+    echo "# -------------------------------------------" 
+    OLDPWD=$PWD
+    cd $1 || exit 1
+    git push github master || exit 1
+    cd $OLDPWD 
 }
 
 # ------------------------------------------------------------------------------
 
 echo ""
-if [ -f repositories.writable ]; then
-    echo "Using: repositories.writable ..."
-    cat repositories.writable | grep -v '^#' | while read A B; do
+if [ -f repositories.push ]; then
+    echo "Using: repositories.push ..."
+    cat repositories.push | grep -v '^#' | while read A B; do
     push_code $A $B || exit 1
     done
 else
--- 03.clean-all.sh
+++ 03.clean-all.sh
@@ -7,12 +7,14 @@ else
     echo "Clean mode: $*"
 fi
 
-cat repositories | grep -v '^#' | while read A B; do
-    if [ -f $A/CMakeClean ]; then
-        echo ""
-        echo "# $B"
-        echo "# -------------------------------------------"  
-        ./$A/CMakeClean $* 
+cat repositories | grep -v '^#' | while read A B C; do
+    echo ""
+    echo "# $B"
+    echo "# -------------------------------------------"  
+    if [ -f $A/CMakeClean.sh ]; then
+        ./$A/CMakeClean.sh $* 
+    else
+        echo "> Nothing to clean ..."
     fi
 done
 
